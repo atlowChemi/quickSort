4b026456b9857f2c720f70581812a7149b4e5987
Chemi Atlow <chemiatlow@gmail.com>
2020-10-02 08:22:49 +0300

    Add styled HR

---
+++ src/components/MyHr.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="hr"></div>
+</template>
+
+<style lang="scss" scoped>
+.hr {
+    height: 0.17rem;
+    width: 100%;
+    background: $primary;
+    background: linear-gradient(to right, $mainLight, $primary 20% 80%, $mainLight);
+    margin: 1rem 0;
+}
+</style>
