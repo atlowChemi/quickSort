500898fba3ec9ddeb9aa4480330174433b84fce7
Chemi Atlow <chemiatlow@gmail.com>
2020-10-02 08:48:49 +0300

    Add single array cell component

---
+++ src/components/ArrayCell.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="cell">{{ item }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+const component = defineComponent({
+    props: {
+        item: {
+            required: true,
+            type: Number,
+        },
+    },
+});
+
+export default component;
+</script>
+
+<style lang="scss" scoped>
+.cell {
+    display: flex;
+    width: 3.5rem;
+    height: 3.5rem;
+    background: $mainDark;
+    color: $mainLight;
+    border: 2px solid $primary;
+    border-radius: 0.4rem;
+    align-items: center;
+    justify-content: center;
+}
+</style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as AppTitle } from "./AppTitle.vue";
+export { default as ArrayCell } from "./ArrayCell.vue";
 export { default as InsertArray } from "./InsertArray.vue";
 export { default as MyHr } from "./MyHr.vue";
