1347604e9ed07cde50b52bd044fe0b35684d8dd1
Chemi Atlow <chemiatlow@gmail.com>
2020-10-04 15:41:26 +0300

    Add Icons globally

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import { Icons } from './components';
 
 const app = createApp(App);
+
+app.component("Icons", Icons)
 
 app.mount('#app')
